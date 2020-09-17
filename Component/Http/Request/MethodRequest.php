3c3d008e4af38a68d2dda7e56d11ed3539fb4f35
mxmoreau <32095501+mxmoreau@users.noreply.github.com>
2020-09-17 18:33:40 +0200

    Create MethodRequest.php

---
+++ Component/Http/Request/MethodRequest.php
@@ -0,0 +1,38 @@
+<?php 
+
+    /**
+     * A set of functions that manage the request method
+     * 
+     * @author Maxime Moreau
+     * @version 1.0.0
+     */
+
+    namespace Library\Component\Http\Request;
+
+    class MethodRequest
+    {
+        const METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'TRACE', 'CONNECT', 'OPTIONS'];
+
+        private static string $method;
+        private static bool $is_method;
+
+        public static function method(): string
+        {
+            return self::$method ??= strtoupper(htmlspecialchars($_SERVER['REQUEST_METHOD'], ENT_QUOTES, 'UTF-8'));
+        }
+
+
+
+        public static function isEqual(string $method): bool
+        {
+            return self::method() === strtoupper($method);
+        }
+
+
+
+        public static function isMethod(string $method = null): bool
+        {
+            return ($method !== null) ? in_array(strtoupper($method), self::METHODS) : self::$is_method ??= in_array(self::method(), self::METHODS);
+        }
+    }
+?>
